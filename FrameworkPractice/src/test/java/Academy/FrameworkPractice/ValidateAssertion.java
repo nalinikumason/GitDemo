6338c7eb15529b9a9712f736a480a0ca913337c2
nalini <nalinikumason@gmail.com>
2021-02-19 12:44:38 -0800

    This is commit by Y person

--- FrameworkPractice/src/test/java/Academy/FrameworkPractice/ValidateAssertion.java
+++ FrameworkPractice/src/test/java/Academy/FrameworkPractice/ValidateAssertion.java
@@ -27,6 +27,7 @@ public class ValidateAssertion extends Base {
 		 L=new LandingPage(driver);
 		//L.getValentineLink().getText();
 		Assert.assertEquals("Valentine's Day Gift Shop", L.getValentineLink().getText());
+	    System.out.println("Text is matching");
 		
 	}
 	
